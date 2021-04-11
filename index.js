b04f52c308c01d15e7320c471adbc938362519ef
Tricia Brooker <grimmi5894@gmail.com>
2021-04-10 23:11:26 -0400

    create index.js, create test for password length, test for password length passes

---
+++ index.js
@@ -0,0 +1,26 @@
+const validatePassword = (password) => {
+// Must be a minimum of 8 characters
+    if (password.length < 8) return false
+// Must contain at least one lower case letter
+
+// Must contain at least one upper case letter
+
+// Must contain at least one numeric value
+
+// Must contain at least one special character
+
+//returns true when all requirements are met
+}
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = validatePassword
